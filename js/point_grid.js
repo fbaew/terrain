555d6e5829bd1f023103eee37eeae6e3b911cade
Gregg Lewis <code@gregglewis.net>
2018-10-24 03:12:14 -0600

    Add simple viewer

---
+++ js/point_grid.js
@@ -0,0 +1,11 @@
+
+
+// 
+// var outerGeometry = new THREE.SphereGeometry( 0.1, 0, 0);
+// var color = Math.random()*16777215;
+// color = color & cmask;       
+// var outerMaterial = new THREE.MeshBasicMaterial( { color: color } );
+// outerMaterial.wireframe = false;
+// var outerShape = new THREE.Mesh( outerGeometry, outerMaterial );
+// outerShape.position.set( x * xf, y * yf, z * zf );
+// scene.add( outerShape );
